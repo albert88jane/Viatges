0db6c5b6de0320c5f87904cd67471f6f0560b749
gerarnan <gerarnan@pm.me>
2020-12-17 10:36:51 +0100

    axios json

--- assets/js/axios.js
+++ assets/js/axios.js
@@ -1,11 +1,11 @@
 // Experiences
 window.onload = function() {
-    axios.get('codebeautify.json', {
+    axios.get('http://localhost/Viatges/assets/js/codebeautify.json', {
     })
     .then(function(info) {
         console.log(info);
   
-        document.getElementsByClassName("card-icon")[0].innerHTML = info.data[0].img;
+        //document.getElementsByClassName("card-icon")[0].innerHTML = info.data[0].img;
   
         document.getElementsByClassName("class-title")[0].innerHTML = info.data[0].titol;
   
