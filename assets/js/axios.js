397025b9cf40279c13db94d84ba2b899af592e34
gerarnan <gerarnan@pm.me>
2020-12-17 10:27:20 +0100

    axios

--- assets/js/axios.js
+++ assets/js/axios.js
@@ -1,15 +1,15 @@
 // Experiences
 window.onload = function() {
-    axios.get('assets/js/codebeautify.json', {
+    axios.get('codebeautify.json', {
     })
     .then(function(info) {
         console.log(info);
   
-        document.getElementsByClassName("card-img-top")[0].innerHTML = info.data.results[0].xp1.img;
+        document.getElementsByClassName("card-img-top")[0].innerHTML = info.data[0].img;
   
-        document.getElementsByClassName("class-title")[0].innerHTML = info.data.results[0].xp1.titol;
+        document.getElementsByClassName("class-title")[0].innerHTML = info.data[0].titol;
   
-        document.getElementsByClassName("card-text")[0].innerHTML = info.data.results[0].xp1.text;
+        document.getElementsByClassName("card-text")[0].innerHTML = info.data[0].text;
     })
     .catch(function(){
         console.log("problem!");
