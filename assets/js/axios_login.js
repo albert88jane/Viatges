66befdfe237af7609b5b64b1bf65df30d91ae716
CarlosErencia <a17gerchifer@inspedralbes.cat>
2021-01-14 11:40:05 +0100

    cambios

--- assets/js/axios_login.js
+++ assets/js/axios_login.js
@@ -1,15 +1,21 @@
 window.onload = function() {
 
-        document.getElementById("boton_login").addEventListener('click', function(){
-         var nom = document.getElementById("nom_log").value;
-         var password = document.getElementById("contrasenya_log").value;
-        })
+    document.getElementById("boton_login").addEventListener('click', function(){
+        var nom = document.getElementById("nom_log").value;
+        var password = document.getElementById("contrasenya_log").value;
+        
 
-    axios.get('http://fastrumbo.alumnes.iam.cat/probes/login_usuari.php', 
-        {   
-        params: {         
-                nom_usuari: nom,
-                contrasenya: password
-        }
+        axios.get('http://fastrumbo.alumnes.iam.cat/probes/login_usuari.php', 
+            {   
+            params: {         
+                    nom_usuari: nom,
+                    contrasenya: password
+            }
+        })
+        .then(function() {
+        })
+        .catch(function(){
+            console.log("problem!");
+        })
     })
 }
